358e6fa8749ee962353752951ec15993543dec8f
CodeBulletin <bmalhotra736@gmail.com>
2022-11-24 21:04:15 +0530

    AA1 fix

--- AA1/AA1.java
+++ AA1/AA1.java
@@ -1,12 +1,14 @@
 package AA1;
 
-@FunctionalInterface
-interface Gravity {
-    public double at(double t);
-}
+// A Gravity Calculator in java to compute the position of an object after falling for 10 seconds, outputting the position in meters. The formula in Math notation is:
+// x(t) = 0.5 × at 2 + vit + xi
+
+// Variable		
+// a Acceleration (m/s 2)       -9.81
+// t Time (s)                   10
+// vi Initial velocity (m/s)    0
 
 public class AA1 {
-
     public static void main(String[] args) {
         double a = 9.81;
         double v = 0;
@@ -16,8 +18,13 @@ public class AA1 {
             return 0.5 * a * t * t + v * t + x;
         };
         
-        double t = 2.0;
+        double t = 10.0;
 
         System.out.printf("Position of ball after time t = %fs is %fm\n", t, earth.at(t));
     }
+}
+
+@FunctionalInterface
+interface Gravity {
+    public double at(double t);
 }
